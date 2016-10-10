4c775188bd3ef71063215454cb96b6e9da75881a
Royston <rlqhenson@hotmail.com>
2016-10-10 16:32:56 +0100

    added post method to named-cat

--- app.rb
+++ app.rb
@@ -17,8 +17,14 @@ get "/random-cat" do
   erb(:index)
 end
 
-get "/named-cat" do
+post "/named-cat" do
   p params
   @name = params[:name]
+  erb(:index)
+end
+
+get "/cat-form" do
+  @cat_form = true
+  @name = ""
   erb(:index)
 end
